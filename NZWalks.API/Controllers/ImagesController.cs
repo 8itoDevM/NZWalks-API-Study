ae2773c1a45bfd3d4944856f52e712a5beb92fdf
Gustavo Marcolino <gustavomarcolino862@gmail.com>
2026-03-06 14:33:15 -0300

    Upload image Domain and Controller created

---
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Controllers {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase {
+        // POST: /api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto request) {
+            ValidadeFileUpload(request);
+
+            if(ModelState.IsValid) {
+                // user repository to upload image
+
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        private void ValidadeFileUpload(ImageUploadRequestDto request) {
+            var allowedExtensions = new string[] { " .jpg ", ".png", "jpeg" };
+
+            if(!allowedExtensions.Contains(Path.GetExtension(request.File.FileName))) {
+                ModelState.AddModelError("file", "Unsupported file extension");
+            }
+
+            if(request.File.Length > 10485760) { // 10MB maximum
+                ModelState.AddModelError("file", "File size exceeds 10MB");
+            }
+        }
+    }
+}
---
+++ NZWalks.API/Models/DTO/ImageUploadRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NZWalks.API.Models.DTO {
+    public class ImageUploadRequestDto {
+        public Guid Id { get; set; }
+
+        [Required]
+        public IFormFile File { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+        
+        public string? FileDescription { get; set; }
+    }
+}
